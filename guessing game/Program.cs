7a6aa9783c122626befbaacd6e224b633459bb23
iqazzal-zarei <gzarei202@gmail.com>
2023-07-20 16:51:45 +0330

    hi game

---
+++ guessing game/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guessing_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretword = "qazal";
+            string guess = "";
+            int wordmax = 3;
+            int wordcount = 0;
+            bool outofguess = false;
+
+            while (guess != secretword && !outofguess)
+            {
+                if (wordcount < wordmax)
+                {
+                    Console.WriteLine(" Entre secretword ");
+                    guess = Console.ReadLine();
+                    wordcount++;
+                }
+                else
+                {
+                    outofguess = true;
+                }
+            }
+                if (outofguess)
+                {
+                    Console.WriteLine(" You lose :[ ");
+                }
+                else
+                {
+                    Console.WriteLine(" You win :] ");
+                }
+                Console.ReadLine();
+
+
+
+
+
+
+            }
+        }
+    }
